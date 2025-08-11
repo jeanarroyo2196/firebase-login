75559279b28b4c8d74451cdff559f9deb79a41a0
jeancarlo2196 <jeancarlo2196@gmail.com>
2025-08-11 17:17:53 -0600

    Added login distribution columns and background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 
+import { Login } from "./components/Login";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,14 @@
+export const Login = () => {
+  return (
+    <main className="w-full flex">
+      <div className="relative flex-1 hidden items-center justify-center h-screen lg:flex">
+        <div
+          className="absolute inset-0 my-auto h-screen bg-[url('src/assets/images/wave.svg')] bg-cover bg-no-repeat"
+        ></div>
+      </div>
+      <div className="flex-1 flex item-center justify-center h-screen">
+
+      </div>
+    </main>
+  );
+};
