d8e2438ef858dc74cc058e430268b919fe140edf
jeancarlo2196 <jeancarlo2196@gmail.com>
2025-08-14 17:00:19 -0600

    Added new login page style

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,39 +1,42 @@
 export const Login = () => {
   return (
-    <main className="w-full flex">
-      <div className="relative flex-1 hidden items-center justify-center h-screen lg:flex">
-        <div
-          className="absolute inset-0 my-auto h-screen bg-[url('src/assets/images/wave.svg')] bg-cover bg-no-repeat"
-        ></div>
+    <main className="w-full flex h-screen">
+      <div className="relative flex-1 hidden lg:flex">
+        <div className="absolute inset-0 bg-[url('src/assets/images/wave.svg')] bg-cover bg-no-repeat"></div>
       </div>
-      <div className="flex-1 flex item-center justify-center h-screen pt-20">
-        <div className="w-full max-w-md space-y-8 px-4 text-white sm:px-0">
-          <div className="pt-24">
-            <div className="mt-5 space-y-2">
-              <h3 className="text-white text-2xl font-bold sm:text-3xl">Log in to your account</h3>
-            </div>
-          </div>
-          <form
-            onSubmit={(e) => e.preventDefault()}
-            className="space-y-5"
-          >
+      <div className="flex-1 flex items-center justify-center bg-gray-900 p-3">
+        <div className="w-full max-w-2xl p-10 rounded-lg shadow border bg-gray-800 border-gray-700">
+          <h1 className="text-3xl font-bold text-white mb-6">
+            Sign in to your account
+          </h1>
+          <form className="space-y-6">
             <div>
-              <label className="font-medium text-white">
+              <label className="block mb-2 text-sm font-medium text-white">
                 Email
               </label>
-              <input type="email" required className="w-full mt-2 px-3 py-2 text-white bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"/>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="w-full mt-2 p-3 bg-gray-700 text-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              />
             </div>
             <div>
-              <label className="font-medium">
+              <label className="block mb-2 text-sm font-medium text-white">
                 Password
               </label>
-              <input type="password" required className="w-full mt-2 px-3 py-2 text-white bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"/>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                className="w-full mt-2 p-3 bg-gray-700 text-white outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+              />
             </div>
-            <button className="w-full mt-2 px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150">
+            <button className="w-full mt-2 px-5 py-2.5 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150 cursor-pointer">
               Sign in
             </button>
+            <p className="text-center text-white">Don't have an account? <a className="font-medium text-indigo-600 hover:text-idnigo-500 cursor-pointer">Sign up</a></p>
           </form>
-          <p className="text-center text-white">Don't have an account? <a className="font-medium text-indigo-600 hover:text-idnigo-500">Sign up</a></p>
         </div>
       </div>
     </main>
