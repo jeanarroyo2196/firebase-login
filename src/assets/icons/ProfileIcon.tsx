5052c2498e58f88d8dab9e2d72233c5f7811e687
jeancarlo2196 <jeancarlo2196@gmail.com>
2025-08-19 22:00:17 -0600

    Added home navbar and sign out functionality

---
+++ src/assets/icons/ProfileIcon.tsx
@@ -0,0 +1,13 @@
+export const ProfileIcon = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="#fff"
+    className="size-8 rounded-full bg-gray-800 outline outline-offset-5"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
+    <path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" />
+  </svg>
+);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,24 @@ import {
   MenuItems,
 } from "@headlessui/react";
 import { CloseButton } from "../assets/icons/CloseButton";
+import { CodeLogo } from "../assets/icons/CodeLogo";
 import { ListButton } from "../assets/icons/ListButton";
+import { NotificationsButton } from "../assets/icons/NotificationsButton";
+import { ProfileIcon } from "../assets/icons/ProfileIcon";
+import firebaseApp from "../config/firebaseConfig.ts";
+import { getAuth, signOut } from "firebase/auth";
 
 const navigation = [
   { name: "Home", href: "#", current: true },
   { name: "About", href: "#", current: false },
   { name: "Contact", href: "#", current: false },
 ];
+
+const auth = getAuth(firebaseApp);
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export const Navbar = () => {
   return (
@@ -32,8 +43,85 @@ export const Navbar = () => {
               <CloseButton aria-hidden="true" />
             </DisclosureButton>
           </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex shrink-0 items-center">
+              <CodeLogo />
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4 pt-1">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={classNames(
+                      item.current
+                        ? "bg-gray-950/50 text-white"
+                        : "text-gray-300 hover:bg-white/5 hover:text-white",
+                      "rounded-md px-3 text-sm font-medium"
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <button
+              type="button"
+              className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
+            >
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">View notifications</span>
+              <NotificationsButton aria-hidden="true" />
+            </button>
+
+            <Menu as="div" className="relative ml-3">
+              <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visibe:outline-indigo-500">
+                <span className="absolutte -inset-1.5" />
+                <span className="sr-only">Open user menu</span>
+                <ProfileIcon />
+              </MenuButton>
+
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+              >
+                <MenuItem>
+                  <a
+                    className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden"
+                    onClick={() => signOut(auth)}
+                  >
+                    Sign out
+                  </a>
+                </MenuItem>
+              </MenuItems>
+            </Menu>
+          </div>
         </div>
       </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? "page" : undefined}
+              className={classNames(
+                item.current
+                  ? "bg-gray-950/50 text-white"
+                  : "text-gray-300 hover:bg-white/5 hover:text-white",
+                "block rounded-md px-3 py-2 text-base font-medium"
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
     </Disclosure>
   );
 };
